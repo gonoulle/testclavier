5370b272894445124819e8f2a498815155318a9b
gonoulle <gonoulle@gmail.com>
2025-04-09 15:10:27 +0200

    Add files via upload

---
+++ test/config.h
@@ -0,0 +1,12 @@
+// Copyright 2021 Nick Brassel (@tzarc)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN B8
+#define I2C1_SDA_PIN B9
+#define I2C1_SCL_PAL_MODE 4
+#define I2C1_SDA_PAL_MODE 4
+#define OLED_DISPLAY_128X64
+#define ENCODER_A_PINS { A15, C3 }
+#define ENCODER_B_PINS { A14, C2 }
---
+++ test/mcuconf.h
@@ -0,0 +1,14 @@
+// Copyright 2023 Nick Brassel (@tzarc)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef STM32_PLLM_VALUE
+#define STM32_PLLM_VALUE 4
+
+#undef STM32_PLLSAIM_VALUE
+#define STM32_PLLSAIM_VALUE 4
+
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 TRUE
